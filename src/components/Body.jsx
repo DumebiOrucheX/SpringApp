485c0ebb331b734e1a5e11c1bd9620572f0c8cc1
DumebiOrucheX <141494928+DumebiOrucheX@users.noreply.github.com>
2025-02-11 22:55:04 +0100

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import Comp1 from "./Comp1"
+import Comp2 from "./Comp2"
+import Comp3 from "./Comp3"
+import Comp4 from "./Comp4"
+import Comp5 from "./Comp5"
+import Comp6 from "./comp6"
+import Footer from "./footer"
+
+
+const Body = () => {
+   return (
+    <>
+      <Comp1 />
+      <Comp2 />
+      <Comp3 />
+      <Comp4 />
+      <Comp5 />
+      <Comp6 />
+      <Footer />
+    </>
+   )
+}
+
+export default Body
